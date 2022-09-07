f2599942f80ad718148c778dde174b8e71445254
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-07 18:51:43 +0430

    Add components to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Col, Container, Row } from "react-bootstrap";
+import AddBook from "./components/AddBook";
+import BooksList from "./components/BooksList";
+
 function App() {
-  return <h1>Hello Firebase</h1>;
+  return (
+    <>
+      <Container style={{ width: "400px" }}>
+        <Row>
+          <Col>
+            <AddBook />
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
+            <BooksList />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default App;
