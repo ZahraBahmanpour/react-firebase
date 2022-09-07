27b9cc78409b264f62172f060f266e6c89246681
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-07 19:06:34 +0430

    remove unwanted from addBook

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,64 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Form, Alert, InputGroup, Button, ButtonGroup } from "react-bootstrap";
-import BookDataService from "../services/book.services";
+import React, { useState } from "react";
+import { Form, Alert, InputGroup, Button } from "react-bootstrap";
 
-const AddBook = ({ id, setBookId }) => {
+const AddBook = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
-  const [status, setStatus] = useState("Available");
-  const [flag, setFlag] = useState(true);
+  const [status, setStatus] = useState(false);
   const [message, setMessage] = useState({ error: false, msg: "" });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setMessage("");
-    if (title === "" || author === "") {
-      setMessage({ error: true, msg: "All fields are mandatory!" });
-      return;
-    }
-    const newBook = {
-      title,
-      author,
-      status,
-    };
-    console.log(newBook);
-
-    try {
-      if (id !== undefined && id !== "") {
-        await BookDataService.updateBook(id, newBook);
-        setBookId("");
-        setMessage({ error: false, msg: "Updated successfully!" });
-      } else {
-        await BookDataService.addBooks(newBook);
-        setMessage({ error: false, msg: "New Book added successfully!" });
-      }
-    } catch (err) {
-      setMessage({ error: true, msg: err.message });
-    }
-
-    setTitle("");
-    setAuthor("");
   };
 
-  const editHandler = async () => {
-    setMessage("");
-    try {
-      const docSnap = await BookDataService.getBook(id);
-      console.log("the record is :", docSnap.data());
-      setTitle(docSnap.data().title);
-      setAuthor(docSnap.data().author);
-      setStatus(docSnap.data().status);
-    } catch (err) {
-      setMessage({ error: true, msg: err.message });
-    }
-  };
-
-  useEffect(() => {
-    console.log("The id here is : ", id);
-    if (id !== undefined && id !== "") {
-      editHandler();
-    }
-  }, [id]);
   return (
     <>
       <div className="p-4 box">
@@ -96,28 +48,13 @@ const AddBook = ({ id, setBookId }) => {
               />
             </InputGroup>
           </Form.Group>
-          <ButtonGroup aria-label="Basic example" className="mb-3">
-            <Button
-              disabled={flag}
-              variant="success"
-              onClick={(e) => {
-                setStatus("Available");
-                setFlag(true);
-              }}
-            >
-              Available
-            </Button>
-            <Button
-              variant="danger"
-              disabled={!flag}
-              onClick={(e) => {
-                setStatus("Not Available");
-                setFlag(false);
-              }}
-            >
-              Not Available
-            </Button>
-          </ButtonGroup>
+          <Form.Check
+            type={"checkbox"}
+            id={`default-checkbox`}
+            label={`available`}
+            checked={status}
+            onChange={() => setStatus((prevStatus) => !prevStatus)}
+          />
           <div className="d-grid gap-2">
             <Button variant="primary" type="Submit">
               Add/ Update
