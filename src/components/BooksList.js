760b1f5ebebde0965c0e17170fb98accf5e1481f
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-07 19:24:32 +0430

    implement getAllBooks

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Table, Button } from "react-bootstrap";
+import BookDataService from "../services/book.services";
 
 const BooksList = () => {
   const [books, setBooks] = useState([]);
+  const getBooks = async () => {
+    const data = await BookDataService.getAllBooks();
+    console.log(data.docs);
+    setBooks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+  useEffect(() => {
+    getBooks();
+  }, []);
   return (
     <>
       <div className="mb-2">
-        <Button variant="dark edit">Refresh List</Button>
+        <Button variant="dark edit" onClick={getBooks}>
+          Refresh List
+        </Button>
       </div>
 
       {/* <pre>{JSON.stringify(books, undefined, 2)}</pre>} */}
